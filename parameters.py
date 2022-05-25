41296ea07f6e862ac137ff79e26b162e58d0019b
picardy <dlekfa00@gmail.com>
2022-05-25 22:43:24 +0900

    00

--- parameters.py
+++ parameters.py
@@ -1,7 +1,26 @@
+# train
+
 --img 640
 --batch 32
 --epochs 500
 --data C:\Users\User\PycharmProjects\Detection\Pothole\data.yaml
 --cfg C:\Users\User\PycharmProjects\Detection\yolov5\models\yolov5s.yaml
 --weights ''
---name pothole result
+--name pothole_result
+
+# validation
+
+--img 640
+--iou 0.65
+--half
+--data C:\Users\User\PycharmProjects\Detection\Pothole\data.yaml
+--weights C:\Users\User\PycharmProjects\Detection\yolov5\runs\train\pothole_results\weights\best.pt
+
+# inference
+
+--img 640
+--conf 0.4
+--half
+--source C:\Users\User\PycharmProjects\Detection\Pothole\test\images
+--weights C:\Users\User\PycharmProjects\Detection\yolov5\runs\train\pothole_results\weights\best.pt
+
