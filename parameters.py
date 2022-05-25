31d29bd655fde7ed6fa6ad68b2a5ac50330916ae
picardy <dlekfa00@gmail.com>
2022-05-25 21:44:12 +0900

    00

--- parameters.py
+++ parameters.py
@@ -1,8 +1,7 @@
 --img 640
 --batch 8
---epochs 100
+--epochs 2
 --data C:\Users\User\PycharmProjects\Detection\Pothole\data.yaml
 --cfg C:\Users\User\PycharmProjects\Detection\yolov5\models\yolov5s.yaml
---weifgts ''
---name pothole_results
---cache
+--weights ''
+--name test
